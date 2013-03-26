1c05903de3e39e915bba63b3db3739fe865a35d6
GaoBin <bin_gao@live.cn>
2013-03-26 10:09:12 +0800

    update the animation for the click event

--- js/EaselJSContactCluster.js
+++ js/EaselJSContactCluster.js
@@ -20,58 +20,52 @@
                 
                 view.showView(chartData);
 			},
-           showView:function (data,offset) {
+           showView:function (data) {
                 var view = this;
                 var $e = view.$el;
+                view.currentContainerName = "currentContainer";
+                view.newContainerName = "newContainer";
+                view.originPoint = "originPoint";
                 
         		var $container = $e.find(".EaselJSContactClusterSummary");
-        		if(typeof offset == "undefined"){
-					offset ={xVal:0,yVal:0};
-					$container.append("<div class='fstCon'><canvas id='ClusterChartCanvas' width='960' height='800'></canvas></div>");
-				}
+				$container.append("<div class='fstCon'><canvas id='ClusterChartCanvas' width='960' height='800'></canvas></div>");
 				
 				//sort the weight
 				var childrenData = data.children;
 				childrenData.sort(weightSort);
 				
-			    var w = 1000,
+			    
+				    
+				var canvas = $e.find("#ClusterChartCanvas")[0];
+				var stage = new createjs.Stage(canvas);
+				view.stage = stage;
+				var container = createContainer.call(view,childrenData);
+				container.name = view.currentContainerName;
+				container.alpha = 1;
+				stage.addChild(container);
+      			stage.update();
+			}
+        });
+        
+        // --------- Private Method --------- //
+        	function createContainer(childrenData){
+        		var view = this;
+      			var stage = view.stage;
+      			var w = 1000,
 				    h = 800,
 				    rx = w / 2,
 				    ry = h / 2;
 				    
-				var canvas = $e.find("#ClusterChartCanvas")[0];
-				var stage = new createjs.Stage(canvas);
-				stage.enableMouseOver(20);
-				var containerRoot = new createjs.Container();
-				
-			    $.each(childrenData,function(i,item){
-					var angle = (360/childrenData.length)*(Math.PI/180)*i;
+     			var containerRoot = new createjs.Container();
+        		//draw the nodes and line
+        		$.each(childrenData,function(i,item){
+					var angle = Math.PI * 2 / childrenData.length * i;
 					var weight = childrenData[i].weight;
 					//the higher weight, the closer the length
 					weight = 10 - weight;
 					
 					var outRx = rx + (Math.cos(angle)*weight*40);
 					var outRy = ry + (Math.sin(angle)*weight*40);
-					
-					var container = new createjs.Container();
-					container.x = outRx;
-					container.y = outRy;
-					container.name = item.name;
-					container.weight = item.weight;
-					
-					//draw the node
-					var node = new createjs.Shape();
-					node.graphics.beginRadialGradientFill(["#FCA000","#F8F28A"],[0,1], 0, 0, 8, 0, 0, 0)
-					                    .drawCircle(0, 0 , 8)
-					                    .beginStroke("#FB4100").drawCircle(0, 0, 9)
-					                    .closePath();
-					
-					container.addChild(node);
-					containerRoot.addChild(container);
-					
-					//add the click event for node
-					container.addEventListener("click", clickEvent);
-					
 					
 					//draw the line
 					var line = new createjs.Shape();
@@ -80,7 +74,22 @@
 						.lineTo(outRx,outRy)
 						.closePath();
 					containerRoot.addChild(line);
-						
+					
+					//draw the node
+					var node = new createjs.Shape();
+					node.graphics.beginRadialGradientFill(["#FCA000","#F8F28A"],[0,1], 0, 0, 8, 0, 0, 0)
+					                    .drawCircle(0, 0 , 8)
+					                    .beginStroke("#FB4100").drawCircle(0, 0, 9)
+					                    .closePath();
+					node.x = outRx;
+					node.y = outRy;
+					node.name = item.name;
+					node.weight = item.weight;
+					containerRoot.addChild(node);
+					
+					//add the click event for node
+					node.addEventListener("click", clickEvent);
+					
 					//show the label
 					var text = new createjs.Text(childrenData[i].name, "12px Arial", "#777");
 					text.x = outRx+10;
@@ -95,59 +104,78 @@
 			    		.drawCircle(rx, ry, 10)
 			    		.beginStroke("#006400").drawCircle(rx, ry, 11)
 			    		.closePath();
-			    
 			    containerRoot.addChild(circle);
-			    stage.addChild(containerRoot);
-			    stage.update();
 			    
 			    function clickEvent(d){
-			    	console.log(d);
-			    	var valX = parseInt(d.target.x - rx);
-			    	var valY = parseInt(d.target.y - ry);
-			    	var offset ={xVal:valX,yVal:valY};
-			    	
-			    	createjs.Ticker.setFPS(500);  
-				    createjs.Ticker.addListener(function() {  
-				    	var container = d.target;
-			    		if(Math.abs(parseInt(containerRoot.x)) != Math.abs(valX)){
-			    			if(valX >= 0){
-					    		containerRoot.x = parseInt(containerRoot.x - 1);
-					    	}else{
-					    		containerRoot.x = parseInt(containerRoot.x + 1);
-					    	}
-			    		}
-			    		
-			    		if(Math.abs(parseInt(containerRoot.y)) != Math.abs(valY)){
-			    			if(valY >= 0){
-					    		containerRoot.y = parseInt(containerRoot.y - 1);
-					    	}else{
-					    		containerRoot.y = parseInt(containerRoot.y + 1);
-					    	}
-			    		}
-			    		
-			    		if(Math.abs(parseInt(containerRoot.x)) == Math.abs(valX) && Math.abs(parseInt(containerRoot.y)) == Math.abs(valY)){
-			    			createjs.Ticker.removeAllListeners();
-			    			
-			    			window.setTimeout(function(){
-					        	var userData = app.transformData(view.dataSet,d.target.name);
-					        	view.showView(userData,offset);
-					       	},app.speed);
-			    		}
-			    		
-				        stage.update();  
-				       
-				    });  
-			    	
-			    	//stage.update();
+			    	//change the origin node and the click node
+			    	containerRoot.removeChild(circle);
+			    	var newCircle = new createjs.Shape();
+				    newCircle.graphics
+				    		.beginRadialGradientFill(["#CCFFCC", "#006400"], [0, 1], 0, 0, 0, 0, 0, 10)
+				    		.drawCircle(0, 0, 10)
+				    		.beginStroke("#006400").drawCircle(0, 0, 11)
+				    		.closePath();
+				    newCircle.x = d.target.x;
+				    newCircle.y = d.target.y;
+      				containerRoot.addChild(newCircle);
+      				
+      				containerRoot.removeChild(d.target);
+      				var node = new createjs.Shape();
+					node.graphics.beginRadialGradientFill(["#FCA000","#F8F28A"],[0,1], 0, 0, 8, 0, 0, 0)
+					                    .drawCircle(0, 0 , 8)
+					                    .beginStroke("#FB4100").drawCircle(0, 0, 9)
+					                    .closePath();
+					node.x = rx;
+					node.y = ry;
+      				containerRoot.addChild(node);
+      				
+      				var userData = app.transformData(view.dataSet,d.target.name);
+      				//sort the weight
+					var childrenData = userData.children;
+					childrenData.sort(weightSort);
+					
+					//add new container
+					var newContainer = createContainer.call(view, childrenData);
+				    newContainer.name = view.newContainerName;
+				    newContainer.x = d.target.x - rx;
+				    newContainer.y = d.target.y - ry;
+				    newContainer.alpha = 0;
+				    stage.addChild(newContainer);
+				    stage.update();
+					
+      				createjs.Ticker.addEventListener("tick", tick);
+      				
+      				function tick(event) {
+				        var oldContainer = stage.getChildByName(view.currentContainerName);
+				        var newContainer = stage.getChildByName(view.newContainerName);
+				        oldContainer.alpha = oldContainer.alpha - 0.1;
+				        newContainer.alpha = newContainer.alpha + 0.1;
+				        
+				        oldContainer.x =  oldContainer.x - (d.target.x - rx) * 0.1;
+				        oldContainer.y =  oldContainer.y - (d.target.y - ry) * 0.1;
+				        newContainer.x =  newContainer.x - (d.target.x - rx) * 0.1;
+				        newContainer.y =  newContainer.y - (d.target.y - ry) * 0.1;
+				        
+				        stage.update(event);
+				        if(oldContainer.alpha <= 0){
+				        	createjs.Ticker.removeEventListener("tick",tick);
+					        //remove oldContainer
+					        newContainer.x = 0;
+					        newContainer.y = 0;
+					        stage.removeChild(oldContainer);
+					        newContainer.name = view.currentContainerName;
+					        newContainer.alpha = 1;
+				          	stage.update(event);
+						}
+      				}
 			    }
+			    
+			    return containerRoot;
+        	}
+        	
+			function weightSort(a,b){
+				return a.weight>b.weight ? 1 :-1;
 			}
-			
-        });
-        
-        // --------- Private Method --------- //
-		function weightSort(a,b){
-			return a.weight>b.weight ? 1 :-1;
-		}
 		// --------- /Private Method --------- //
 
     })(jQuery);
