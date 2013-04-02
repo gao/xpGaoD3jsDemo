da25697ba4f68a8f4d697a37217ca8775fe2a071
GaoBin <bin_gao@live.cn>
2013-04-02 17:26:52 +0800

    clean the code and update the style

--- js/EaselJSForceCluster.js
+++ js/EaselJSForceCluster.js
@@ -24,6 +24,7 @@
                 var $e = view.$el;
                 view.currentContainerName = "currentContainer";
                 view.newContainerName = "newContainer";
+                view.cName = "centerCircle";
                 view.rootName = data.name;
 				
 				createjs.Ticker.useRAF = true;
@@ -70,7 +71,6 @@
 						}
 					}
 				}
-
 				
       			var stage = view.stage;
 				var rx = originPoint.x;
@@ -129,7 +129,7 @@
 			        containerRoot.addChild(node);
 			        
 			       	//add the click event for node
-					node.addEventListener("click", clickEvent);
+					node.addEventListener("click", function(d){clickEvent.call(view,d)});
 
 			        //show the label
 			        var text = createText.call(view,cx,cy, cData.name);
@@ -139,122 +139,40 @@
 				//draw the origin point
 				if(showChildrenLevel){
 					var circle = createCenterCircle.call(view);
-				   	circle.name = parentName;
+				   	circle.name = view.cName;
 				   	circle.children = childrenData.length;
 			      	circle.x = rx;
 			      	circle.y = ry;
 			      	containerRoot.addChild(circle);
 			      	
 			      	//add the click event for circle
-					circle.addEventListener("click", clickOriginPointEvent);
+					circle.addEventListener("click", function(d){clickOriginPointEvent.call(view,d)});
 			      	
 				    var text = createText.call(view,rx,ry, parentName);
 	      			containerRoot.addChild(text); 
 				}
-				
-			   
-			    
-			    function clickEvent(d){
-			    	//change the origin node and the click node
-			    	var stage = view.stage;
-			    	view.rootName = d.target.name;
-			    	
-			    	var statLayout = stage.getChildByName(view.currentContainerName);
-			    	statLayout.removeChild(circle);
-			    	var newCircle = new createjs.Shape();
-			    	var newCircle = createCenterCircle.call(view);
-				    newCircle.x = d.target.x;
-				    newCircle.y = d.target.y;
-      				statLayout.addChild(newCircle);
-      				
-      				statLayout.removeChild(d.target);
-      				var node = createNodeCircle.call(view,true);
-					node.x = rx;
-					node.y = ry;
-      				statLayout.addChild(node);
-      				
-      				app.ContactDao.getByName(d.target.name).done(function(userData){
-      					view.chartData = userData;
-						
-						//add new container
-						var newContainer = createContainer.call(view, userData, {x:view.canvasW/2, y: view.canvasH/2}, true);
-					    newContainer.name = view.newContainerName;
-					    newContainer.x = d.target.x - rx;
-					    newContainer.y = d.target.y - ry;
-					    newContainer.alpha = 0;
-					    stage.addChild(newContainer);
-					    
-					    stage.update();
-					    
-						createjs.CSSPlugin.install();
-						var $contactInfo = view.$el.find(".contact-info");
-				   		if($contactInfo.find("span").size() > 0){
-				   			var leftVal = $contactInfo.position().left - (d.target.x - rx);
-				   			var topVal = $contactInfo.position().top - (d.target.y - ry);
-				   			createjs.Tween.get($contactInfo[0]).to({opacity : 0.1, left : leftVal, top : topVal }, app.speed,createjs.Ease.quartInOut).call(function(){
-				   				$contactInfo.empty();
-				   			});
-				   		}
-					      	
-					    var ox = statLayout.x - (d.target.x - rx);
-					    var oy = statLayout.y - (d.target.y - ry);
-					      	
-					    createjs.Tween.get(statLayout).to({alpha : 0, x : ox, y : oy }, app.speed,createjs.Ease.quartInOut); 
-					      	
-					    createjs.Tween.get(newContainer).to({alpha : 1, x : 0, y : 0}, app.speed,createjs.Ease.quartInOut).call(function() {
-					      	createjs.Ticker.removeEventListener("tick",stage);
-					        //remove oldContainer
-						    newContainer.x = 0;
-						    newContainer.y = 0;
-						    stage.removeChild(statLayout);
-						    newContainer.name = view.currentContainerName;
-						    newContainer.alpha = 1;
-					        stage.update();
-					     }); 
-						
-	      				createjs.Ticker.addEventListener("tick", stage);
-					    
-					});		
-			    }
-			    
-			    function clickOriginPointEvent(d){
-			    	var name = d.target.name;
-			    	var children = d.target.children;
-			    	
-			    	var $contactInfo = view.$el.find(".contact-info");
-			   		
-			    	if($contactInfo.find("span").size() == 0){
-			    		$contactInfo.html('<span class="label label-info">'+name+": "+children+' friends</span>')
-				    	$contactInfo.css("top",d.target.y-10);
-				    	$contactInfo.css("left",d.target.x+20);
-				    	$contactInfo.css("opacity",1);
-			    	}else{
-			    		$contactInfo.empty();
-			    	}
-			    	
-			    }
 			    
 			    return containerRoot;
         	}
         	
         	function createNodeCircle(showChildrenLevel){
-		      	var r = 7;
+		      	var r = 4;
 		      	var color = "#d9eefe";
 		    	if(showChildrenLevel){
 		    		color = "#0B95B1";
 		    	}
 		      	var circle = new createjs.Shape();
+		      		circle.graphics.beginStroke("#a4998e").drawCircle(0, 0, r+0.5);
 		      		circle.graphics.beginFill(color).drawCircle(0, 0, r);
-		      		circle.graphics.beginStroke("#979ca3").drawCircle(0, 0, r+1);
-		      		circle.graphics
 		      	return circle;
 		    }
 		    
 		    function createCenterCircle(){
-		      	var r = 7;
+		      	var r = 6;
 		      	var circle = new createjs.Shape();
-		      		circle.graphics.beginStroke("#a4998e").drawCircle(0, 0, r+1);
+		      		circle.graphics.beginStroke("#a4998e").drawCircle(0, 0, r+0.5);
 		      		circle.graphics.beginFill("#ffe9c2").drawCircle(0, 0, r);
+		      		
 		      	return circle;
 		    }
 		    
@@ -274,6 +192,90 @@
 		      		text.y = y0 + 10;
 		      	return text;
 		    }
+		    
+		    function clickEvent(d){
+		    	var view = this;
+			    //change the origin node and the click node
+			    var stage = view.stage;
+			    view.rootName = d.target.name;
+			    var rx = view.originPoint.x;
+			    var ry = view.originPoint.y;
+			    	
+			    var statLayout = stage.getChildByName(view.currentContainerName);
+			    var oldCenterCircle = statLayout.getChildByName(view.cName);
+			    statLayout.removeChild(oldCenterCircle);
+			        
+			    var newCircle = new createjs.Shape();
+			    var newCircle = createCenterCircle.call(view);
+				    newCircle.x = d.target.x;
+				    newCircle.y = d.target.y;
+				    newCircle.name = view.cName;
+      			statLayout.addChild(newCircle);
+      				
+      			statLayout.removeChild(d.target);
+      			var node = createNodeCircle.call(view,true);
+					node.x = rx;
+					node.y = ry;
+      			statLayout.addChild(node);
+      				
+      			app.ContactDao.getByName(d.target.name).done(function(userData){
+      				view.chartData = userData;
+						
+					//add new container
+					var newContainer = createContainer.call(view, userData, {x:view.canvasW/2, y: view.canvasH/2}, true);
+					    newContainer.name = view.newContainerName;
+					    newContainer.x = d.target.x - rx;
+					    newContainer.y = d.target.y - ry;
+					    newContainer.alpha = 0;
+					stage.addChild(newContainer);
+					    
+					stage.update();
+					    
+					createjs.CSSPlugin.install();
+					var $contactInfo = view.$el.find(".contact-info");
+				   	if($contactInfo.find("span").size() > 0){
+				   		var leftVal = $contactInfo.position().left - (d.target.x - rx);
+				   		var topVal = $contactInfo.position().top - (d.target.y - ry);
+				   		createjs.Tween.get($contactInfo[0]).to({opacity : 0.1, left : leftVal, top : topVal }, app.speed,createjs.Ease.quartInOut).call(function(){
+				   			$contactInfo.empty();
+				   		});
+				   	}
+					      	
+					var ox = statLayout.x - (d.target.x - rx);
+					var oy = statLayout.y - (d.target.y - ry);
+					      	
+					createjs.Tween.get(statLayout).to({alpha : 0, x : ox, y : oy }, app.speed,createjs.Ease.quartInOut); 
+					      	
+					createjs.Tween.get(newContainer).to({alpha : 1, x : 0, y : 0}, app.speed,createjs.Ease.quartInOut).call(function() {
+					    createjs.Ticker.removeEventListener("tick",stage);
+					    //remove oldContainer
+						newContainer.x = 0;
+						newContainer.y = 0;
+						stage.removeChild(statLayout);
+						newContainer.name = view.currentContainerName;
+						newContainer.alpha = 1;
+						stage.update();
+					}); 
+						
+	      			createjs.Ticker.addEventListener("tick", stage);
+					    
+				});		
+			}
+			    
+			function clickOriginPointEvent(d){
+				var view = this;
+			    var children = d.target.children;
+			    var $contactInfo = view.$el.find(".contact-info");
+			   		
+			    if($contactInfo.find("span").size() == 0){
+			    	$contactInfo.html('<span class="label label-info">'+view.rootName+": "+children+' friends</span>')
+				    $contactInfo.css("top",d.target.y-10);
+				    $contactInfo.css("left",d.target.x+20);
+				    $contactInfo.css("opacity",1);
+			    }else{
+			    		$contactInfo.empty();
+			    }
+			}
         	
 			function weightSort(a,b){
 				return a.weight>b.weight ? 1 :-1;
